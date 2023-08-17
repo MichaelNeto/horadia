81398e16c73a3d289ed31bd355658831cf1c98b6
Michael Neto <michael.neto00@gmail.com>
2023-08-17 07:35:17 -0300

    relogio

---
+++ script.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+   var hora = 8
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        //Bom dia!!
+        
+        img.src = 'manha.png'
+        document.body.style.background = '#E9E1E0'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde!!
+        img.src = 'tarde.png'
+        document.body.style.background = '#C1C4D5'
+    } else {
+        //Boa noite 
+        img.src = 'noite.png'
+        document.body.style.background = '#5C5F6F'
+    }
+        
+
+
+}
